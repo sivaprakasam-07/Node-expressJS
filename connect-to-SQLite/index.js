e6635311f1481f8653a729bb8f08d467fd71473b
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-12-10 22:04:43 +0530

    wrote the code for the practice of api to getbooks from db

--- connect-to-SQLite/index.js
+++ connect-to-SQLite/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
-
+const { open } = require('sqlite');
+const path = require('path');
+const sqlite3 = require('sqlite3');
+const dbPath = path.join(__dirname, 'goodreads.db');
 const app = express();
 
+let db = null;
+
+const initializedbAndServer = async () => {
+    try {
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database
+        });
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000")
+        });
+    } catch (e) {
+        console.log(`DB Error: ${e.message}`);
+        process.exit(1);
+    }
+};
+
+app.get('/books/', async (req, res) => {
+    const getBooks = `SELECT * FROM book ORDER BY book_id;`;
+    const booksArray = await db.all(getBooks);
+    response.send(booksArray);
+})
+initializedbAndServer();
 
 
-
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-});
