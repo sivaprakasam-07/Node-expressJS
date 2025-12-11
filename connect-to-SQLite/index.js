465f81ab1604fe37901f98c5882ec2f73ab08b4a
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-12-11 23:51:42 +0530

    completed the db learn

--- connect-to-SQLite/index.js
+++ connect-to-SQLite/index.js
@@ -1,54 +1,76 @@
 const express = require('express')
-const path = require('path')
-
 const { open } = require('sqlite')
+const path = require('path')
 const sqlite3 = require('sqlite3')
-
-const app = express()
 const dbPath = path.join(__dirname, 'goodreads.db')
+const app = express()
+
+app.use(express.json)
 
 let db = null
 
-const initializeDBAndServer = async () => {
+const initializedbAndServer = async () => {
     try {
         db = await open({
             filename: dbPath,
             driver: sqlite3.Database,
         })
-        app.listen(3000, () => {
-            console.log('Server Running at PORT 3000')
+        app.listen(4000, () => {
+            console.log('Server is running on port 4000')
         })
     } catch (e) {
         console.log(`DB Error: ${e.message}`)
         process.exit(1)
     }
 }
-initializeDBAndServer()
 
-// Get Books API
-app.get('/books/', async (request, response) => {
-    const getBooksQuery = `
-    SELECT
-      *
-    FROM
-      book
-    ORDER BY
-      book_id;`
-    const booksArray = await db.all(getBooksQuery)
-    response.send(booksArray)
+app.get('/books/', async (req, res) => {
+    const getBooks = `SELECT * FROM book ORDER BY book_id;`
+    const booksArray = await db.all(getBooks)
+    res.send(booksArray)
 })
 
-app.get('/books/:bookId/', async (req, res) => {
-    const { bookId } = req.params;
-    const getBookQuery = `
-    SELECT
-      *
-    FROM
-      book
-    WHERE
-      book_id=${bookId};`
-    const book = await db.get(getBookQuery);
-    res.send(book);
+app.get('/books/:bookId', async (req, res) => {
+    const { bookId } = req.params
+    const getBooks_id = `SELECT * FROM book WHERE book_id=${bookId};`
+    const book = await db.get(getBooks_id)
+    res.send(book)
 })
 
+app.post('/books/', async (req, res) => {
+    const bookDetails = req.body
+    const {
+        title,
+        authorId,
+        rating,
+        ratingCount,
+        reviewCount,
+        description,
+        pages,
+        dateOfPublication,
+        editionLanguage,
+        price,
+        onlineStores,
+    } = bookDetails
+    const addBookQuery = `
+    INSERT INTO
+      book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+    VALUES
+      (
+        '${title}',
+         ${authorId},
+         ${rating},
+         ${ratingCount},
+         ${reviewCount},
+        '${description}',
+         ${pages},
+        '${dateOfPublication}',
+        '${editionLanguage}',
+         ${price},
+        '${onlineStores}'
+      );`
 
+    const addBook = await db.run(addBookQuery)
+    res.send(addBook)
+})
+initializedbAndServer()
